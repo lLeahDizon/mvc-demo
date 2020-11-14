eb397135d868d5b7273317484b9293d32a997bc2
Lemon <516330797@qq.com>
2020-11-14 23:38:18 +0800

    设计全局样式

--- src/app1.js
+++ src/app1.js
@@ -1,1 +1,12 @@
 import "./app1.css";
+import $ from "jquery";
+
+const $button1 = $("#add1");
+const $button2 = $("#minus1");
+const $button3 = $("#mul2");
+const $button4 = $("#divide2");
+const $number = $("#number");
+const n = localStorage.getItem("n");
+$number.text(n || 100);
+
+$button1.on("click", () => {});
